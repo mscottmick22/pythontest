abf4409eb771e584afc33870f63ea3875ac4e2ae
Michael <mscottmick22@gmail.com>
2024-05-01 13:20:09 +0100

    another files

---
+++ another.py
@@ -0,0 +1,20 @@
+# BRANCH TEST ADDED
+# Taking user input# Taking user input
+number = int(input("Enter a number: "))
+
+# Checking if the number is positive, negative, or zero
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+number = int(input("Enter a number: "))
+
+# Checking if the number is positive, negative, or zero
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
